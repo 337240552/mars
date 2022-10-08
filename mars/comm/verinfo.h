1840c9b767b2c4e03fd4332c09a3c4f11e757052
mingchunhu <mingchunhu@tencent.com>
2022-10-08 15:09:38 +0800

    feat: 最低支持android-18

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "da0f571d"
+#define MARS_REVISION "e4a90f0e"
 #define MARS_PATH "sirius"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-09-05 11:45:57"
+#define MARS_BUILD_TIME "2022-10-08 14:57:23"
 #define MARS_TAG ""
 
 #endif
