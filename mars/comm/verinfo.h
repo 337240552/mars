b1d391d917dab29c5a8db8ff6f6ff49ea4b23336
mingchunhu <mingchunhu@tencent.com>
2023-02-22 17:26:04 +0800

    feat: TLog去腾讯化，重命名包名

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e4a90f0e"
+#define MARS_REVISION "1840c9b7"
 #define MARS_PATH "sirius"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-10-08 14:57:23"
+#define MARS_BUILD_TIME "2023-02-22 09:53:23"
 #define MARS_TAG ""
 
 #endif
