65f8604090a6f51bca899d0363b43e19f164786f
mingchunhu <mingchunhu@tencent.com>
2023-02-28 19:20:13 +0800

    feat: 添加x86支持

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e4a90f0e"
-#define MARS_PATH "sirius"
+#define MARS_REVISION "1840c9b7"
+#define MARS_PATH "x64"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-10-08 14:57:23"
+#define MARS_BUILD_TIME "2023-02-28 19:15:02"
 #define MARS_TAG ""
 
 #endif
